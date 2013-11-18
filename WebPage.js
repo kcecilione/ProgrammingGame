a21de91148f6450f9e3ed4a6ba194a933060a54a
kcecilione <kristen.cecilione1@marist.edu>
2013-11-18 18:07:52 -0500

    Update WebPage2 and Others Pages
    WebPage2 now links to various pages such as HowtoPlay and UserInfo.
    WebPage.js is still unfinished

--- WebPage.js
+++
@@ -1,7 +0,0 @@
-function beginGame(){
-	var name = document.getElementById('nameBox').value;
-		document.getElementById('outputDiv').innerHTML=
-			'Hello ' + name + '!';
-document.getElementById('outputDiv').style.display='block';
-document.getElementById('inputDiv').style.display='none';
-}
