d71c6983c1dbcb709bf4a0669d1e51f63e4ab1ac
kcecilione <kristen.cecilione1@marist.edu>
2013-11-20 15:12:43 -0500

    Comments and Functions

--- WebPage.js
+++ WebPage.js
@@ -18,3 +18,20 @@ function goToBeach(){
 function findProtection(){
 	alert('In order to find protection, you need to find the map, located on the beach!')
 }
+
+function talkOnBeach(){
+	document.getElementById('outputDiv').innerHTML=
+		'Hello. My name is Holokai. Are you looking for the map?'
+}
+
+function forestFromBeach(){
+
+}
+
+function talkInCave(){
+
+}
+
+function forestFromCave(){
+
+}
